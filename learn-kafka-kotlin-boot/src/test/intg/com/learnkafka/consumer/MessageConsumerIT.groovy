2a8e89a41368f4a3d25f24148a381a11c923dea3
Dilip Sundarraj <dilip.sundarraj@target.com>
2019-08-21 10:09:39 -0500

    added  the code to add countdown latch in integration test cases

--- learn-kafka-kotlin-boot/src/test/intg/com/learnkafka/consumer/MessageConsumerIT.groovy
+++ learn-kafka-kotlin-boot/src/test/intg/com/learnkafka/consumer/MessageConsumerIT.groovy
@@ -1,6 +1,6 @@
 package com.learnkafka.consumer
 
-
+import com.learnkafka.consmer.MessageConsumer
 import com.learnkafka.consmer.MessageConsumerRetryListener
 import com.learnkafka.service.MessageService
 import org.apache.kafka.clients.producer.ProducerRecord
@@ -14,12 +14,16 @@ import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.TestPropertySource
 import spock.lang.Specification
+import spock.util.concurrent.PollingConditions
+
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 @DirtiesContext
 @EmbeddedKafka(ports = [9092], topics = ["\${spring.kafka.consumer.topic}"])
-@TestPropertySource(properties = ["spring.kafka.retry.generate-alert-retry-threshold = 3" ,
+@TestPropertySource(properties = ["spring.kafka.retry.generate-alert-retry-threshold = 4",
         "spring.kafka.retry.backoff.initial-interval=500"])
 class MessageConsumerIT extends Specification {
 
@@ -33,45 +37,47 @@ class MessageConsumerIT extends Specification {
     private KafkaTemplate<String, String> template;
 
     @SpringBean
-    MessageService messageService = Mock()
+    MessageService messageServiceMock = Mock(MessageService.class)
+
+    @Autowired
+    MessageConsumer messageConsumer
 
     def "Integration test for MessageConsumer"() {
 
-        when:
+        given:
         ProducerRecord<String, String> producerRecord = new ProducerRecord<>("test-topic", "my-aggregate-id", message)
+        def latch = new CountDownLatch(1)
+
+        when:
         template.send(producerRecord)
-        boolean result = true
-        Thread.sleep(sleepTime)
+        latch.await(3, TimeUnit.SECONDS)
 
         then:
-        result == true
-        count * messageService.processMessage(_)
+        count * messageServiceMock.processMessage(message) >> null
 
         where:
-        message | count | sleepTime
-        "3"     | 1     | 3000
+        message | count
+        "3"     | 1
 
     }
 
     def "Integration test for MessageConsumer - Exception Scenario"() {
 
         given:
-        messageService.processMessage(_) >> { throw new RuntimeException("Exception thrown") }
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("test-topic", "my-aggregate-id", message)
+        messageServiceMock.processMessage(_) >> { throw new RuntimeException("Exception thrown") }
+        def latch = new CountDownLatch(1)
 
         when:
-        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("test-topic", "my-aggregate-id", message)
         template.send(producerRecord)
-        boolean result = true
-        Thread.sleep(sleepTime)
+        latch.await(3, TimeUnit.SECONDS)
 
         then:
-        result == true
-        retriedCount == messageConsumerRetryListener.retryCount
-        1 * messageService.processRecovery(_)
+        count * messageServiceMock.processRecovery(_)
 
         where:
-        message | retriedCount | sleepTime
-        "5"     | 3            | 3000
+        message | count
+        "5"     | 1
 
     }
 }
